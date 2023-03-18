8f7469ec543ba92caa10bfcb8f86224ea70d40a8
GabrielaGarciaR <gaby091097@gmail.com>
2023-03-17 22:48:49 -0600

    Se dio formato al archivo

--- Ejercicio2Condicionales.java
+++ Ejercicio2Condicionales.java
@@ -4,42 +4,41 @@ public class Ejercicio2Condicionales {
 
         System.out.println("/********Condicional IF ELSE****************/");
 
-        int numeroif=-4;
-        if (numeroif>0){
+        int numeroif = -4;
+        if (numeroif > 0) {
             System.out.println("El numero es positivo");
-        }else if(numeroif<0){
+        } else if (numeroif < 0) {
             System.out.println("El numero es negativo");
-        }
-        else{
+        } else {
             System.out.println("El numero es igual que 0");
         }
 
         System.out.println("/********Bucle WHILE****************/");
 
-        int numeroWhile=0;
-        while (numeroWhile <3){
-           System.out.println(numeroWhile);
-           numeroWhile++;
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
         }
 
         System.out.println("/********Bucle DO WHILE****************/");
 
-        numeroWhile=0;
+        numeroWhile = 0;
         do {
             System.out.println(numeroWhile);
-        } while(numeroWhile>1);
+        } while (numeroWhile > 1);
 
         System.out.println("/********Bucle FOR***************/");
 
-        for(int numeroFor=0; numeroFor<=3;numeroFor++){
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
             System.out.println(numeroFor);
         }
 
         System.out.println("/********SWITCH***************/");
 
-        String estacion="Primavera";
+        String estacion = "Primavera";
 
-        switch (estacion){
+        switch (estacion) {
             case "Primavera":
                 System.out.println("Es primavera");
                 break;
